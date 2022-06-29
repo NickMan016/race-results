52581c0d8dc3f999747c7aeb194ae3b363623a90
NickMan016 <npcastillo73@gmail.com>
2022-06-28 20:12:10 -0500

    Aplicando Provider de Countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './assets/css/index.css';
+import { CountriesProvider } from './context/CountriesDB/CountriesProvider';
 import { F1Provider } from './context/F1DB/F1Provider';
 import { Router } from './routes';
 
 function App() {
   return (
     <F1Provider>
-      <Router />
+      <CountriesProvider>
+        <Router />
+      </CountriesProvider>
     </F1Provider>
   );
 }
