6fe0483e0b456eac5ea4311a867cc219b769972e
NickMan016 <npcastillo73@gmail.com>
2022-06-28 20:08:20 -0500

    Creación de Provider F1

--- src/context/F1DB/F1Context.tsx
+++ src/context/F1DB/F1Context.tsx
@@ -1,3 +1,9 @@
 import { createContext } from "react";
+import { MRData } from "../../interfaces/F1Interfaces";
 
-export const F1Context = createContext({});
+export type F1ContextProps = {
+    stateRaces: MRData,
+    getResults: ( query: string ) => void
+}
+
+export const F1Context = createContext<F1ContextProps>({} as F1ContextProps);
--- src/context/F1DB/F1Provider.tsx
+++ src/context/F1DB/F1Provider.tsx
@@ -1,13 +1,45 @@
+import { useState } from "react"
+import apiF1DB from "../../api/F1DB"
+import { MRData } from "../../interfaces/F1Interfaces"
 import { F1Context } from "./F1Context"
+// import { F1Reducer } from "./F1Reducer"
 
 interface ProviderProps {
     children: JSX.Element | JSX.Element[]
 }
 
+const INITIAL_STATE_RACES: MRData = {
+    xmlns: "",
+    series: "",
+    url: "",
+    limit: "",
+    offset: "",
+    total: "",
+    RaceTable: {
+        season: "",
+        round: "",
+        Races: []
+    }
+}
+
 export const F1Provider = ({ children }: ProviderProps) => {
+
+    // const [stateRaces, dispatch] = useReducer(F1Reducer, INITIAL_STATE_RACES);
+    const [stateRaces, setStateRaces] = useState(INITIAL_STATE_RACES);
+
+    const getResults = async ( query: string ) => {
+        
+        const response: any = await apiF1DB(query)
+        const data: MRData = response.data.MRData;
+        setStateRaces(data)        
+    }
+
     return (
-        <F1Context.Provider value={{}}>
-            { children }
+        <F1Context.Provider value={{
+            stateRaces,
+            getResults
+        }}>
+            {children}
         </F1Context.Provider>
     )
 }
