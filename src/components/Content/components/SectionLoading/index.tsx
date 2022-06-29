e3e059a034f8ddfe51df17374ced5b83e3a33df1
NickMan016 <npcastillo73@gmail.com>
2022-06-28 20:10:15 -0500

    Componente de carga

---
+++ src/components/Content/components/SectionLoading/index.tsx
@@ -0,0 +1,10 @@
+export const SectionLoading = () => {
+    return (
+        <div className="seccion">
+            <div className="skeleton skeleton__texto skeleton__titulo"></div>
+            <div className="skeleton skeleton__texto"></div>
+            <div className="skeleton skeleton__texto"></div>
+            <div className="skeleton skeleton__texto skeleton__cuerpo"></div>
+        </div>
+    )
+}
