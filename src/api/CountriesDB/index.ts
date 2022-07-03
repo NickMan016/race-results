b4c1d0e3e6a8cdf7d9660cb588610bead19b83fc
NickMan016 <npcastillo73@gmail.com>
2022-07-03 13:44:59 -0500

    Cambio en peticiones de las API's

--- src/api/CountriesDB/index.ts
+++ src/api/CountriesDB/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function apiCountriesDB( url: string ) {
     try {
-        const response = await axios.get(`https://restcountries.com/v2/${url}`);
+        const response = await axios.get(`https://restcountries.com/v2/${url}?fields=name,alpha3Code,altSpellings,region,capital,timezones,flags`);
 
         return response;
     } catch (error) {
