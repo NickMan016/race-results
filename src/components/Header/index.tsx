0353f6c4dc5d0a1f4b983eaca56a5c666269d513
NickMan016 <npcastillo73@gmail.com>
2022-07-15 18:48:15 -0500

    Cambios de Responsive al Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,21 +2,24 @@ import './Header.css';
 import logo from './../../assets/img/Race_Results_Logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 export const Header = () => {
+    const [activeMenu, setActiveMenu] = useState(false);
+
     return (
         <header>
             <nav className="navbar">
                 <a href="#" className="logo">
-                    <img src={ logo } alt="Logo" />
+                    <img src={logo} alt="Logo" />
                 </a>
-                <ul>
-                    {/* <li><a href="#">Drivers</a></li>
+                <ul className={`${activeMenu ? 'active' : ''}`}>
+                    <li><a href="#">Drivers</a></li>
                     <li><a href="#">Constructors</a></li>
                     <li><a href="#">Circuits</a></li>
-                    <li><a href="#">Races</a></li> */}
-                    <li><a href="#"><FontAwesomeIcon icon={faBars} /></a></li>
+                    <li><a href="#">Races</a></li>
                 </ul>
+                <button className='btn__lista btn__menu' id='btn-menu'><FontAwesomeIcon icon={faBars} onClick={() => setActiveMenu(!activeMenu)} /></button>
             </nav>
         </header>
     )
