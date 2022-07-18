03d5ccf9009f2cd6cad564b5be6081d13d12d0c9
NickMan016 <npcastillo73@gmail.com>
2022-07-18 14:03:05 -0500

    Cambio en TryCatch de API

--- src/api/CountriesDB/index.ts
+++ src/api/CountriesDB/index.ts
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-export default async function apiCountriesDB( url: string ) {
-    try {
-        const response = await axios.get(`https://restcountries.com/v2/${url}?fields=name,alpha3Code,altSpellings,region,capital,timezones,flags`);
-
-        return response;
-    } catch (error) {
-        Promise.reject(error);
-    }
+export default function apiCountriesDB( url: string ) {
+        return axios.get(`https://restcountries.com/v2/${url}?fields=name,alpha3Code,altSpellings,region,capital,timezones,flags`);
 }
--- src/api/F1DB/index.ts
+++ src/api/F1DB/index.ts
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-export default async function apiF1DB( url: string ) {
-    try {
-        const response = await axios.get(`https://ergast.com/api/f1/${url}.json`);
-
-        return response;
-    } catch (error) {
-        console.log(error);
-    }
+export default function apiF1DB( url: string ) {
+    return axios.get(`https://ergast.com/api/f1/${url}.json`);
 }
