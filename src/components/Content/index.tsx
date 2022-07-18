8cb00fd12795413c8b0a7b47f68f5edf04c05333
NickMan016 <npcastillo73@gmail.com>
2022-07-18 12:47:35 -0500

    Removiendo importaciones sin usar

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,15 +1,12 @@
 import './Content.css'
 import { SectionResults } from './components/SectionResults';
 import { SectionRace } from './components/SeccionRace';
-import { useContext } from 'react';
-import { F1Context } from '../../context/F1DB/F1Context';
 import { SectionDriverStanding } from './components/SectionDriverStanding';
 import { SectionConstructorStanding } from './components/SectionConstructorStanding';
 import { SectionFlags } from './components/SectionFlags';
 import data from './../../data/data.json';
 
 export const Content = () => {
-    
     return (
         <div className="contenedor grid__contenedor">
             <div className="principal__contenedor">
