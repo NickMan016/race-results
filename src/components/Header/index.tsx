be0bd01bd9fee251787917aa5c6dc3f81f9b309b
NickMan016 <npcastillo73@gmail.com>
2022-07-22 15:21:36 -0500

    Rutas de navegación en el Menú

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import logo from './../../assets/img/Race_Results_Logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faBars } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Header = () => {
     const [activeMenu, setActiveMenu] = useState(false);
@@ -10,14 +11,14 @@ export const Header = () => {
     return (
         <header>
             <nav className="navbar">
-                <a href="#" className="logo">
+                <Link to="/" className="logo">
                     <img src={logo} alt="Logo" />
-                </a>
+                </Link>
                 <ul className={`${activeMenu ? 'active' : ''}`}>
-                    <li><a href="#">Drivers</a></li>
-                    <li><a href="#">Constructors</a></li>
-                    <li><a href="#">Circuits</a></li>
-                    <li><a href="#">Races</a></li>
+                    <li><NavLink to="/drivers">Drivers</NavLink></li>
+                    <li><NavLink to="/">Constructors</NavLink></li>
+                    <li><NavLink to="/">Circuits</NavLink></li>
+                    <li><NavLink to="/">Races</NavLink></li>
                 </ul>
                 <button className='btn__lista btn__menu' id='btn-menu'><FontAwesomeIcon icon={faBars} onClick={() => setActiveMenu(!activeMenu)} /></button>
             </nav>
