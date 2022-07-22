a531b9d0c0a697606595853b8519f5b18942a01c
NickMan016 <npcastillo73@gmail.com>
2022-07-22 15:20:15 -0500

    Ruta para Pilotos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Content } from "../components/Content";
+import { Drivers } from "../components/Drivers";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
@@ -9,6 +10,7 @@ export const Router = () => {
             <Header />
             <Routes>
                 <Route path="/" element={ <Content /> } />
+                <Route path="/drivers" element={ <Drivers /> } />
             </Routes>
             <Footer />
         </BrowserRouter>
