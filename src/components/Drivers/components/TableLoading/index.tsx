24616ba0d15d1bd0716e0b28affff0f9bca8b495
NickMan016 <npcastillo73@gmail.com>
2022-07-22 15:19:49 -0500

    Componente de carga de tabla

---
+++ src/components/Drivers/components/TableLoading/index.tsx
@@ -0,0 +1,68 @@
+import './TableLoading.css';
+
+export const TableLoading = () => {
+    return(
+        <tbody>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+            <tr className="skeleton__fila">
+                <td></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+                <td><div className="skeleton skeleton__texto"></div></td>
+            </tr>
+        </tbody>
+    )
+}
