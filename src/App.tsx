041622bf383a584ba2fb8c649a91a5fac780b47c
NickMan016 <npcastillo73@gmail.com>
2022-08-01 21:04:56 -0500

    Agregando Tailwind al proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './assets/css/index.css';
 import { CountriesProvider } from './context/CountriesDB/CountriesProvider';
 import { F1Provider } from './context/F1DB/F1Provider';
 import { Router } from './routes';
+import './index.css'
 
 function App() {
   return (
