dc1e4da24fca84e94ec25d1807f44495f091ee7b
NickMan016 <npcastillo73@gmail.com>
2022-08-03 09:59:17 -0500

    Acomodado de importaciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './assets/css/index.css';
 import { CountriesProvider } from './context/CountriesDB/CountriesProvider';
 import { F1Provider } from './context/F1DB/F1Provider';
 import { Router } from './routes';
+import './assets/css/index.css';
 import './index.css'
 
 function App() {
