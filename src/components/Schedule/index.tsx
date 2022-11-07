6bd0933736f8dff7fd87d445b5694811583de498
NickMan016 <npcastillo73@gmail.com>
2022-11-07 16:49:23 -0600

    Cambio en formato de las fechas

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -82,46 +82,46 @@ export const Schedule = () => {
                                                     </div>
                                                 </div>
                                                 <div>
-                                                    <FontAwesomeIcon className="inline-block mr-2" icon={faCalendarDays} />
-                                                    <FormatDate gmt={true} date={`${RaceTable?.Races[0].date}`} time={`${RaceTable?.Races[0].time}`} region={region} capital={capital} />
-                                                </div>
-                                                <div>
-                                                    <b>Practice 1 -</b>&nbsp;
-                                                    <FormatDate date={`${RaceTable?.Races[0].FirstPractice.date}`} time={`${RaceTable?.Races[0].FirstPractice.time}`} region={region} capital={capital} />
+                                                    <b>FP1: </b>
+                                                    <FormatDate date={`${RaceTable?.Races[0].FirstPractice.date}`} time={`${RaceTable?.Races[0].FirstPractice.time}`} />
                                                 </div>
                                                 {
                                                     (Object.keys(RaceTable?.Races[0].Sprint || {}).length) === 0 ? (
                                                         <>
                                                             <div>
-                                                                <b>Practice 2 -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].SecondPractice.date}`} time={`${RaceTable?.Races[0].SecondPractice.time}`} region={region} capital={capital} />
+                                                                <b>FP2: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].SecondPractice.date}`} time={`${RaceTable?.Races[0].SecondPractice.time}`} />
                                                             </div>
                                                             <div>
-                                                                <b>Practice 3 -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].ThirdPractice?.date}`} time={`${RaceTable?.Races[0].ThirdPractice?.time}`} region={region} capital={capital} />
+                                                                <b>FP3: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].ThirdPractice?.date}`} time={`${RaceTable?.Races[0].ThirdPractice?.time}`} />
                                                             </div>
                                                             <div>
-                                                                <b>Qualifying -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].Qualifying.date}`} time={`${RaceTable?.Races[0].Qualifying.time}`} region={region} capital={capital} />
+                                                                <b>Qualifying: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].Qualifying.date}`} time={`${RaceTable?.Races[0].Qualifying.time}`} />
                                                             </div>
                                                         </>
                                                     ) : (
                                                         <>
                                                             <div>
-                                                                <b>Qualifying -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].Qualifying.date}`} time={`${RaceTable?.Races[0].Qualifying.time}`} region={region} capital={capital} />
+                                                                <b>Qualifying: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].Qualifying.date}`} time={`${RaceTable?.Races[0].Qualifying.time}`} />
                                                             </div>
                                                             <div>
-                                                                <b>Practice 2 -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].SecondPractice.date}`} time={`${RaceTable?.Races[0].SecondPractice.time}`} region={region} capital={capital} />
+                                                                <b>FP2: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].SecondPractice.date}`} time={`${RaceTable?.Races[0].SecondPractice.time}`} />
                                                             </div>
                                                             <div>
-                                                                <b>Sprint -</b>&nbsp;
-                                                                <FormatDate date={`${RaceTable?.Races[0].Sprint?.date}`} time={`${RaceTable?.Races[0].Sprint?.time}`} region={region} capital={capital} />
+                                                                <b>Sprint: </b>
+                                                                <FormatDate date={`${RaceTable?.Races[0].Sprint?.date}`} time={`${RaceTable?.Races[0].Sprint?.time}`} />
                                                             </div>
                                                         </>
                                                     )
                                                 }
+                                                <div>
+                                                    <b>Race: </b>
+                                                    <FormatDate date={`${RaceTable?.Races[0].date}`} time={`${RaceTable?.Races[0].time}`} />
+                                                </div>
                                             </div>
                                         ) : (
                                             <div className="col-span-2 sm:col-span-1">
--- src/hooks/FormatDate.tsx
+++ src/hooks/FormatDate.tsx
@@ -1,27 +1,14 @@
 import Moment from "react-moment"
 
 interface propsHook {
-    gmt?: boolean
     date: string
     time: string
-    region: string
-    capital: string
 }
 
-export const FormatDate = ({ gmt = false, date, time, region, capital }: propsHook) => {    
+export const FormatDate = ({ date, time }: propsHook) => {    
     return (
         <>
-            <Moment date={`${date}T${time}`} format="DD/MM/YYYY HH:mm" />
-            &nbsp;hrs
-            {
-                gmt ? (
-                    <>
-                        &nbsp;(GMT
-                        <Moment date={date} format="Z" />)
-                    </>
-                ) : undefined
-            }
-
+            <Moment date={`${date}T${time}`} format="ddd, MMM DD HH:mm [hrs]" />
         </>
     )
 }
